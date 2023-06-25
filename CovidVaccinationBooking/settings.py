6607e30182702b3beb729fb3b0f81216d6644082
Lonelydeveloper <dhanushkumarganapathy@gmail.com>
2023-06-25 22:53:45 +0530

    set debug to true

--- CovidVaccinationBooking/settings.py
+++ CovidVaccinationBooking/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '%yt2&2^l9%u_8yekcix*@8m@p06v4k_)ar2s#*13+s4p*z5773'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
